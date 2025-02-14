49225a60973cd56ac2678902927dcedc9a5a02b1
asm2me <49681638+asm2me@users.noreply.github.com>
2025-02-14 21:01:26 +0200

    Update nimble_server.h

--- nimble_server.h
+++ nimble_server.h
@@ -39,11 +39,27 @@ public:
         text_sensors_.push_back(text_sensor);
     }
     // Add callback functions
-//    void set_on_read(std::function<void(std::string, size_t)> callback) { read_callback_ = callback; }
+//  void set_on_read(std::function<void(std::string, size_t)> callback) { read_callback_ = callback; }
     void set_on_receive(std::function<void(std::string, size_t)> callback) { receive_callback_ = callback; }
 
     // Callback members
     std::function<void(std::string, size_t)> receive_callback_;
+
+    void add_on_client_connected_callback(std::function<void(std::string)> callback) {
+        client_connected_callback_.add(std::move(callback));
+    }
+    void add_on_state_callback(std::function<void()> &&f)
+    {
+        this->state_callback_.add(std::move(f));
+    }
+    void add_on_user_selected_callback(std::function<void(std::string)> callback) {
+        this->user_selected_callback_.add(std::move(callback));
+    }
+
+
+    void add_on_user_command_callback(std::function<void(std::string, std::string)> callback) {
+        user_command_callback_.add(std::move(callback));
+    }
 
     void start_advertising();
     void stop_advertising();
@@ -53,15 +69,6 @@ public:
     bool is_active() const { return this->state_ != STATE_STOPPED; }
     bool is_accepted() const { return this->state_ == STATE_PIN_ACCEPTED; }
     bool is_rejected() const { return this->state_ == STATE_PIN_REJECTED; }
-    void add_on_state_callback(std::function<void()> &&f) { this->state_callback_.add(std::move(f)); }
-    void add_on_user_selected_callback(std::function<void(std::string)> callback) {
-        this->user_selected_callback_.add(std::move(callback));
-    }
-
-    void add_on_user_command_callback(std::function<void(std::string)> callback) {
-      this->user_command_callback_.add(std::move(callback));
-    }
-
     std::string get_userid() const { return this->user_id_; };
     std::string get_cmd() const { return this->cmd_id_; };
 
@@ -69,9 +76,11 @@ public:
     void set_user_commands(const std::string commands);
     std::string user_id_;
     std::string cmd_id_;
+
     CallbackManager<void()> state_callback_{};
     CallbackManager<void(std::string)> user_selected_callback_;
-    CallbackManager<void(std::string)> user_command_callback_;
+    CallbackManager<void(std::string)> client_connected_callback_;
+    CallbackManager<void(std::string, std::string)> user_command_callback_;  // Change to two parameters
 
 protected:
     bool should_start_{false};
